6903c60b1e070f4302148c95cccae7fbe354a45f
Vishal Pattnaik <vishalpattnaik387@gmail.com>
2019-07-17 17:20:54 +0530

    fixed issues

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -19,40 +19,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        BeanFactory factory = new XmlBeanFactory(new ClassPathResource("bean.xml"));
-//        Movie movie1 = factory.getBean("movie1", Movie.class);
-//        Movie movie2 = factory.getBean("movie2", Movie.class);
-//        System.out.println("-------Bean Factory------");
-//        System.out.println(movie1.getActor());
-//        System.out.println(movie2.getActor());
-//
-//        BeanDefinitionRegistry registry = new XmlBeanFactory(new ClassPathResource("bean.xml"));
-//        XmlBeanDefinitionReader rdr = new XmlBeanDefinitionReader(registry);
-//        System.out.println("\n\n-------Bean Definition Registry------");
-//        Movie mov1 = ((XmlBeanFactory)factory).getBean("movie1", Movie.class);
-//        Movie mov2 = ((XmlBeanFactory)factory).getBean("movie2", Movie.class);
-//        System.out.println(mov1.getActor());
-//        System.out.println(mov2.getActor());
 
-//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
-//        Movie movie11 = applicationContext.getBean("movie", Movie.class);
-//        System.out.println("\n\n-------Application Context------");
-//        System.out.println(movie11);
-//        Movie movie22 = applicationContext.getBean("movie", Movie.class);
-//        System.out.println(movie22);
-//
-//        System.out.println("\n\n--------beanA == beanB--------");
-//        System.out.println(movie11 == movie22);
-//
-//        BeanLifecycleDemoBean beanLifecycleDemoBean = applicationContext.getBean("beanlifecycledemo", BeanLifecycleDemoBean.class);
-//        beanLifecycleDemoBean.afterPropertiesSet();
-//        beanLifecycleDemoBean.destroy();
-
+        //ApplicationContext to retrieve details
+        
        AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
         applicationContext.registerShutdownHook();
 
-     //   BeanPostProcessorDemoBean beanPostProcessorDemoBean = applicationContext.getBean("beanpostprocessordemobean", BeanPostProcessorDemoBean.class);
-     //   beanPostProcessorDemoBean.postProcessBeforeInitialization(beanPostProcessorDemoBean, beanpostprocessordemobean);
+    
 
     }
 
